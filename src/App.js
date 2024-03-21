aaf404ab6191f65c85ad07380c5f9d90ac4d2145
Amarnathamraj <amarnath987654@gmail.com>
2024-03-21 21:13:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+// Write your code at relevant places in the code below
+
+import Expenses from './components/Expenses/Expenses';
+
+
+const App=()=> {
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
+  return (
+    <div>
+      <h1>Let's get Started</h1>
+        
+      <Expenses expenses={expenses}></Expenses>
+    </div>
+  );
+}
+
+export default App;
